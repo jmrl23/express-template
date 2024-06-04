ca7174e67eadd71064c925579d5ac6077c04751b
Jomariel Gaitera <jojogaitera@gmail.com>
2024-06-04 23:12:58 +0800

    Optimize ✨✨

--- src/lib/constant/environment.ts
+++ src/lib/constant/environment.ts
@@ -2,6 +2,6 @@ import env from 'env-var';
 
 export const NODE_ENV = env.get('NODE_ENV').default('development').asString();
 
-export const SERVER_HOSTNAME = env.get('SERVER_HOSTNAME').default('localhost').asString()
+export const SERVER_HOST = env.get('SERVER_HOST').default('0.0.0.0').asString();
 
 export const PORT = env.get('PORT').default(3001).asPortNumber();
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,16 @@
 import './init';
-import { SERVER_HOSTNAME, PORT } from './lib/constant/environment';
+import { SERVER_HOST, PORT } from './lib/constant/environment';
 import detectPort from 'detect-port';
 import server from './server';
+import * as c from 'colorette';
 import logger from './lib/util/logger';
 
 async function main() {
-  const host = SERVER_HOSTNAME;
+  const host = SERVER_HOST;
   const port = await detectPort(PORT);
 
   server.listen({ host, port }, function () {
-    const serverAddress = server.address();
-    if (!serverAddress || typeof serverAddress === 'string') return;
-    const address =
-      serverAddress.family === 'IPv6'
-        ? `[${serverAddress.address}]`
-        : serverAddress.address;
-    const _port = [80, 443].includes(port) ? '' : `:${port}`;
-    const protocol = [80, 443].includes(port) ? 'https://' : 'http://';
-    logger.info(`${protocol}${address}${_port}`);
+    logger.info(`Running on port ${c.bold(c.yellow(port))}`);
   });
 }
 
