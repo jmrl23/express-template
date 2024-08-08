688b2913fc6d7846289a01fe3db109618e441593
Jomariel Gaitera <gaiterajomariel@gmail.com>
2024-08-09 00:50:44 +0800

    feat(plugin): sync api version with package.json

--- src/plugins/swagger.ts
+++ src/plugins/swagger.ts
@@ -1,39 +1,48 @@
-import { OpenAPIV3_1 } from 'openapi-types';
+import { OpenAPIV3, OpenAPIV3_1 } from 'openapi-types';
 import swaggerUiExpress from 'swagger-ui-express';
 import { asPlugin } from '../lib/common';
 import { paths } from '../lib/swagger';
+import fs from 'node:fs';
+import path from 'node:path';
+
+interface Options {
+  routePrefix: string;
+}
 
 export default asPlugin(async function swagger(app, options: Options) {
   const { routePrefix } = options;
+  const packageJson: Record<string, unknown> = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, '../../package.json')).toString(),
+  );
+  const version: string =
+    typeof packageJson.version === 'string' ? packageJson.version : '0.0.0';
+
+  const servers: OpenAPIV3_1.ServerObject[] = [
+    {
+      url: 'http://localhost:3001',
+      description: 'Default local development server',
+    },
+  ];
+
+  const spec: OpenAPIV3_1.Document = {
+    openapi: '3.0.0',
+    info: {
+      title: 'Rest API',
+      version,
+    },
+    servers,
+    paths,
+    components: {
+      // securitySchemes: {
+      //   bearerAuth: {
+      //     type: 'http',
+      //     scheme: 'bearer',
+      //   },
+      // },
+    },
+  };
 
   app
     .use(routePrefix, swaggerUiExpress.serve)
     .get(routePrefix, swaggerUiExpress.setup(spec));
 });
-
-const spec: OpenAPIV3_1.Document = {
-  openapi: '3.0.0',
-  info: {
-    title: 'Rest API',
-    version: '0.0.1',
-  },
-  servers: [
-    {
-      url: 'http://localhost:3001',
-      description: 'Default local development server',
-    },
-  ],
-  paths,
-  components: {
-    // securitySchemes: {
-    //   bearerAuth: {
-    //     type: 'http',
-    //     scheme: 'bearer',
-    //   },
-    // },
-  },
-};
-
-interface Options {
-  routePrefix: string;
-}
