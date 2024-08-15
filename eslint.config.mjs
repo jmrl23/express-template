826ef32cbaa45a2eb60f670dae28963f875aa462
Jomariel Gaitera <gaiterajomariel@gmail.com>
2024-08-15 08:24:05 +0800

    refactor: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['src/**/*.{js,ts}'] },
   {
     languageOptions: {
       globals: { ...globals.node },
