749686bf5f273dc2d59e1b531ce9d60200a5eb3a
Jomariel Gaitera <gaiterajomariel@gmail.com>
2024-09-21 15:11:46 +0800

    refactor(env): update .env

--- src/lib/constant/env.ts
+++ src/lib/constant/env.ts
@@ -1,3 +1,5 @@
 import env from 'env-var';
 
 export const PORT = env.get('PORT').default(3001).asPortNumber();
+
+export const CORS_ORIGIN = env.get('CORS_ORIGIN').asArray(',');
--- src/plugins/middlewares.ts
+++ src/plugins/middlewares.ts
@@ -2,24 +2,18 @@ import cors from 'cors';
 import express from 'express';
 import pinoHttp from 'pino-http';
 import { asPlugin, logger } from '../lib/common';
+import { CORS_ORIGIN } from '../lib/constant/env';
 
 export default asPlugin(async function (app) {
   app.use(
     pinoHttp({
       logger,
-      customLogLevel(_, response, error) {
-        if (response.statusCode >= 400 && response.statusCode < 500) {
-          return 'warn';
-        } else if (response.statusCode >= 500 || error) {
-          return 'error';
-        } else if (response.statusCode >= 300 && response.statusCode < 400) {
-          return 'silent';
-        }
-        return 'info';
-      },
       wrapSerializers: false,
     }),
-    cors({ origin: '*' }),
+  );
+
+  app.use(
+    cors({ origin: CORS_ORIGIN }),
     express.json({ strict: true }),
     express.urlencoded({ extended: true }),
   );
